99442d2d5080cd9a8abce51d93e4fb7b9c2595b3
unknown <nikkevanukas1995@gmail.com>
2023-12-15 07:18:39 +0200

    feat: palce ships toggle

--- main.gd
+++ main.gd
@@ -8,6 +8,8 @@ var cell_size: int = 50
 @export var HORIZONTAL_SHIP: Vector2 = Vector2(1,0)
 @export var VERTICAL_SHIP: Vector2 = Vector2(0,1)
 
+var is_place_ships: bool = false
+
 var ships: Array
 
 # Called when the node enters the scene tree for the first time.
@@ -18,16 +20,15 @@ func new_game():
 	generate_ships()
 
 func generate_ships():
-	ships.clear()
-	for i in range(100):
-		add_ship(Vector2(i,(i-1) % 2), HORIZONTAL_SHIP, 1)
-	#add_ship(Vector2(7,3), VERTICAL_SHIP, 4)
+	pass
 
 func add_ship(start: Vector2, direction: Vector2, length: int):
+	print(start, direction, length)
 	for i in range(length):
 		add_segment(start + direction * i)
 
 func add_segment(segment_position: Vector2):
+	print("poop",segment_position)
 	ships.append(segment_position)
 	var shipSegment = ship_scene.instantiate()
 	shipSegment.position = (segment_position * cell_size) + Vector2(0, cell_size)
@@ -36,3 +37,9 @@ func add_segment(segment_position: Vector2):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_place_ships_button_pressed():
+	is_place_ships = is_place_ships != true
+	print(is_place_ships)
+	
