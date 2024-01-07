eb96ac9489a8cbcf1c599f2393729dde7f4d1c41
unknown <nikkevanukas1995@gmail.com>
2024-01-07 13:27:52 +0200

    feat: disable marker selector

--- hud/hud.gd
+++ hud/hud.gd
@@ -9,9 +9,11 @@ signal marked_changed
 
 func _ready():
 	$PlaceShipsButton.button_pressed = is_place_ships
+	$MarkerSelector.set_disabled(is_place_ships)
 
 func _on_place_ships_button_pressed():
 	is_place_ships = !is_place_ships
+	$MarkerSelector.set_disabled(is_place_ships)
 	toggle_place_ships.emit()
 
 func _on_selector_item_selected(index):
