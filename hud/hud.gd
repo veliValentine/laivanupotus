b4ef7dcf1cf087b14b5f3963d799c7dbd4d48c0e
unknown <nikkevanukas1995@gmail.com>
2024-01-07 16:00:22 +0200

    fix: scaling

--- hud/hud.gd
+++ hud/hud.gd
@@ -9,8 +9,17 @@ signal new_game_started
 @export var selected_marker_id = -1
 
 func _ready():
+	place_children_positions()
+	
 	$PlaceShipsButton.button_pressed = is_place_ships
 	$MarkerSelector.set_disabled(is_place_ships)
+	$NewGameDialog.visible = false
+
+func place_children_positions():
+	$PlaceShipsButton.position = Vector2(0, 660)
+	$MarkerSelector.position = Vector2(Settings.SCREEN_WIDTH / 2 - 75, 660)
+	$NewGameButton.position = Vector2(Settings.SCREEN_WIDTH - 200, 660)
+	$NewGameDialog.set_position(Vector2(Settings.SCREEN_WIDTH / 2 - 150, 330 - 75))
 
 func _on_place_ships_button_pressed():
 	is_place_ships = !is_place_ships
--- settings/settings.gd
+++ settings/settings.gd
@@ -5,8 +5,3 @@ var SCREEN_HEIGHT: int = 720
 
 var CELLS: int = 10
 var CELL_SIZE: int = 50
-
-func _ready():
-	var screen_size = get_viewport().size
-	SCREEN_WIDTH = screen_size.x
-	SCREEN_HEIGHT = screen_size.y
